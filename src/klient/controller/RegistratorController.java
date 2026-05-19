bad06722af9817e716a7c3b7554db34172b3b1e7
Kristoffer1710 <kristoffer.arefjord@gmail.com>
2026-05-19 18:26:19 +0200

    Add RegistratorController and KlientMain classes; initialize socket communication

--- src/klient/controller/RegistratorController.java
+++ src/klient/controller/RegistratorController.java
@@ -1,5 +1,13 @@
 package klient.controller;
 
-public class RegistratorController {
-    
+import java.io.IOException;
+
+import common.*;
+import klient.KlientMain;
+
+public class RegistratorController extends KlientMain {
+    public Melding opprettHenvendelse(HenvendelseType type, Object innhold) throws IOException, ClassNotFoundException {
+        Melding foresporsel = new Melding(Kommando.OPPRETT_HENVENDELSE, innhold, type);
+        return sendOgMotta(foresporsel);
+    }
 }
