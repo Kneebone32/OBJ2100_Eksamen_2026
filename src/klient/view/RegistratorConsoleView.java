f1a7738b664164a627123974e632dafc2459fb30
Brainslug <sanderalgry@outlook.com>
2026-05-19 13:52:30 +0200

    Add RegistratorConsoleView for inquiry management
    Implement console view for registering inquiries with a menu.

---
+++ src/klient/view/RegistratorConsoleView.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class RegistratorConsoleView {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void displayMenu(){
+        System.out.println("Velkommen");
+        System.out.println();
+        System.out.println("1.Opprett ny henvendelse");
+        System.out.println("2.Kanseller henvendelse");
+        System.out.println("3.Avslutt");
+        System.out.println();
+        System.out.print("Velg: ");
+
+        String valg = scanner.nextLine();
+
+        switch(valg){
+            case "1":
+              opprettHenvendelse();
+              break;
+
+            case "2":
+                kansellerHenvendelse();
+                break;
+
+            case "3":
+                System.out.println("Velkommen igjen");
+                return;
+
+            default:
+                System.out.println("Ugyldig valg");
+        }
+    }
+
+
+    private void opprettHenvendelse() {
+        System.out.print("Beskrivelse av henvendelse: ");
+        String hendelseInput = scanner.nextLine();
+    }
+
+    private void kansellerHenvendelse(){
+        System.out.print("Hendelse_id som skal kanselleres: ");
+        int id = Integer.parseInt(scanner.nextLine());
+    }
+}
