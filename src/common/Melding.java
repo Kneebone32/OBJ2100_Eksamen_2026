086d51cdeeadda3f848257aced0a222e2d0065df
Kneebone <kayern3@hotmail.com>
2026-05-19 18:18:51 +0200

    Melding og KlientHandler

--- src/common/Melding.java
+++ src/common/Melding.java
@@ -7,23 +7,44 @@ public class Melding implements Serializable {
     private final Kommando kommando;
     private final int henvendelseID;
     private final Object innhold;
-    private final String responsStatus;
+    private final SvarKode responsStatus;
+    private final HenvendelseType henvendelseType;
 
     //Konstruktøren for meldingene hvor vi har oppgitt kommando, innhold, responsstatus
     //og en henvendelseID som skal være unik for hver melding.
-    public Melding(Kommando kommando, Object innhold, String responsStatus, int henvendelseID) {
+    public Melding(Kommando kommando, Object innhold, SvarKode responsStatus, int henvendelseID) {
         this.kommando = kommando;
         this.innhold = innhold;
         this.responsStatus = responsStatus;
         this.henvendelseID = henvendelseID;
+        this.henvendelseType = null;
     }
 
+    //brukes som svarmelding fra server
+    public Melding(Object innhold, SvarKode responsStatus){
+        this.innhold = innhold;
+        this.responsStatus = responsStatus;
+        henvendelseID = 0;
+        kommando = null;
+        this.henvendelseType = null;
+    }
+
+    //kan brukes til opprettelse av henvendelse
+    public Melding(Kommando kommando, Object innhold, HenvendelseType henvendelseType){
+        this.kommando = kommando;
+        this.henvendelseType = henvendelseType;
+        this.innhold = innhold;
+        this.responsStatus = null;
+        this.henvendelseID = 0;
+    }
+
+
     //De forskjellige getters for å hente ut informasjonen i meldingene.
-    public Kommando getCommand() {
+    public Kommando getKommando() {
         return kommando;
     }
 
-    public int getHenvendelseInnhold(){
+    public int getHenvendelseID(){
         return henvendelseID;
     }
 
@@ -31,8 +52,12 @@ public class Melding implements Serializable {
         return innhold;
     }
 
-    public String getResponseStatus() {
+    public SvarKode getResponseStatus() {
         return responsStatus;
+    }
+
+    public HenvendelseType getHenvendelseType(){
+        return henvendelseType;
     }
 
 }
--- src/server/KlientHandler.java
+++ src/server/KlientHandler.java
@@ -20,26 +20,54 @@ public class KlientHandler implements Runnable {
             ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
 
             while (true) {
-                Melding request = (Melding) input.readObject();
-                Melding response = håndterForespørsel(request);
-                output.writeObject(response);
+                Melding forespørsel = (Melding) input.readObject();
+                Melding svar = håndterForespørsel(forespørsel);
+                output.writeObject(svar);
                 output.flush();
             }
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (IOException | InterruptedException | ClassNotFoundException e) {
             System.out.println("Feil ved deserialisering av melding: " + e.getMessage());
         }
     }
-
-    private Melding håndterForespørsel(Melding request) throws InterruptedException {
-        switch (request.getCommand()) {
+        //TODO: unntakshåntering på typetvang
+    private Melding håndterForespørsel(Melding forespørsel) throws InterruptedException {
+        switch (forespørsel.getKommando()) {
             case OPPRETT_HENVENDELSE:
-                Henvendelse nyHenvendelse = (Henvendelse) request.getInnhold();
+                Henvendelse nyHenvendelse = (Henvendelse) forespørsel.getInnhold();
                 int id = server.opprettHenvendelse(nyHenvendelse);
-                return new Melding(Kommando.OPPRETT_HENVENDELSE, null, "OK", id);
+                return new Melding(Kommando.OPPRETT_HENVENDELSE, null, SvarKode.SUKSESS, id);
 
             case HENT_HENVENDELSE_STATUS:
-                int hentId = (int) request.getInnhold();
-                Henvendelse henvendelse = server.hentHenvendelse(hentId);
+                int hentId = (int) forespørsel.getInnhold();
+                HenvendelseStatus henvendelseStatus = server.hentHenvendelseStatus(hentId);
+                return new Melding(henvendelseStatus, SvarKode.SUKSESS);
+
+
+
+            case KANSELLER_HENVENDELSE:
+                int henvendelseID = (int) forespørsel.getInnhold();
+                SvarKode svarKode = server.kansellerHenvendelse();
+                if(svarKode == SvarKode.ERROR){
+                    return new Melding("Kunne ikke kansellere henvendelse", svarKode);
+                }
+                return new Melding("Henvendelse ble kansellert", svarKode);
+
+            case HENT_LEDIG_HENVENDELSE:
+                int nesteHenvendelseID = server.hentNesteHenvendelseID();
+                Henvendelse henvendelse = server.hentHenvendelse(nesteHenvendelseID);
+                return new Melding(henvendelse, SvarKode.SUKSESS);
+
+            case SETT_HENVENDELSE_FULLFØRT:
+            
+            default: 
+                return new Melding("Ukjent forespørsel", SvarKode.ERROR);
+
         }
+        
+    }
+
+
+    private boolean validerRolle(){
+        
     }
 }
