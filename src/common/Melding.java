e2ccf47182b30fc89bf62f2e4fc63eae296f684d
Kneebone <kayern3@hotmail.com>
2026-05-20 11:26:00 +0200

    KlientRolle

--- src/common/Melding.java
+++ src/common/Melding.java
@@ -3,6 +3,7 @@ package common;
 import java.io.Serializable;
 
 import common.enums.HenvendelseType;
+import common.enums.KlientRolle;
 import common.enums.Kommando;
 import common.enums.SvarKode;
 
@@ -13,15 +14,18 @@ public class Melding implements Serializable {
     private final Object innhold;
     private final SvarKode responsStatus;
     private final HenvendelseType henvendelseType;
+    private final KlientRolle klientRolle;
 
     //Konstruktøren for meldingene hvor vi har oppgitt kommando, innhold, responsstatus
     //og en henvendelseID som skal være unik for hver melding.
-    public Melding(Kommando kommando, Object innhold, SvarKode responsStatus, int henvendelseID, HenvendelseType henvendelseType) {
+    public Melding(Kommando kommando, Object innhold, SvarKode responsStatus, int henvendelseID, HenvendelseType henvendelseType, KlientRolle klientRolle) {
         this.kommando = kommando;
         this.innhold = innhold;
         this.responsStatus = responsStatus;
         this.henvendelseID = henvendelseID;
         this.henvendelseType = henvendelseType;
+        this.klientRolle = klientRolle;
+        
     }
 
     //De forskjellige getters for å hente ut informasjonen i meldingene.
@@ -45,25 +49,29 @@ public class Melding implements Serializable {
         return henvendelseType;
     }
 
-    //metoder for å skille på opprettHenvendelse, forespørsel og svar.
-    public static Melding opprettHenvendelse(String innhold, HenvendelseType henvendelseType){
-        return new Melding(Kommando.OPPRETT_HENVENDELSE, innhold, null, 0, henvendelseType);
+    public KlientRolle getKlientRolle() {
+        return klientRolle;
     }
 
-    public static Melding forespørsel(Kommando kommando, Object innhold){
-        return new Melding(kommando, innhold, null, 0, null);
+    //metoder for å skille på opprettHenvendelse, forespørsel og svar.
+    public static Melding opprettHenvendelse(String innhold, HenvendelseType henvendelseType, KlientRolle klientRolle){
+        return new Melding(Kommando.OPPRETT_HENVENDELSE, innhold, null, 0, henvendelseType, klientRolle);
+    }
+
+    public static Melding forespørsel(Kommando kommando, Object innhold, KlientRolle klientRolle){
+        return new Melding(kommando, innhold, null, 0, null, klientRolle);
     }
 
     public static Melding svar(Object innhold, SvarKode svarKode){
-        return new Melding(null, innhold, svarKode, 0, null);
+        return new Melding(null, innhold, svarKode, 0, null, null);
     }
 
     public static Melding svarMedID(Object innhold, SvarKode svarKode, int id){
-        return new Melding(null, innhold, svarKode, id, null);
+        return new Melding(null, innhold, svarKode, id, null, null);
     }
 
     public static Melding registrerKlient(Kommando kommando, Object klientRolle){
-        return new Melding(kommando, klientRolle, null, 0, null);
+        return new Melding(kommando, klientRolle, null, 0, null, null);
     }
 
 }
