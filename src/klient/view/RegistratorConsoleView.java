a309df0139e5fe2c441c4b8c336e3f14d2a1c66f
Brainslug <sanderalgry@outlook.com>
2026-05-20 09:18:46 +0200

    Update menu options and integrate controller methods

--- src/klient/view/RegistratorConsoleView.java
+++ src/klient/view/RegistratorConsoleView.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class RegistratorConsoleView {
 
     private final Scanner scanner = new Scanner(System.in);
+    private final RegistratorController controller;
 
     public void displayMenu(){
+        while (true){
         System.out.println("Velkommen");
         System.out.println();
         System.out.println("1.Opprett ny henvendelse");
         System.out.println("2.Kanseller henvendelse");
-        System.out.println("3.Avslutt");
+        System.out.println("3.sjekk status ");
+        System.out.println("4.Avslutt");
         System.out.println();
         System.out.print("Velg: ");
 
@@ -25,12 +28,17 @@ public class RegistratorConsoleView {
                 break;
 
             case "3":
+                sjekkHenvendelseStatus();
+                break;
+
+            case "4":
                 System.out.println("Velkommen igjen");
                 return;
 
             default:
                 System.out.println("Ugyldig valg");
         }
+    }
     }
 
 
@@ -69,21 +77,26 @@ public class RegistratorConsoleView {
         System.out.print("Beskrivelse av henvendelse: ");
         String hendelseInput = scanner.nextLine();
 
-        //String respons = controller.submit(type,hendelseInput);
+        String melding = controller.submit(type,hendelseInput);
+        System.out.println(melding);
     }
 
     //Registrator kan kansellere en henvendelse
     private void kansellerHenvendelse(){
         System.out.print("Hendelse_id som skal kanselleres: ");
         int id = Integer.parseInt(scanner.nextLine());
+
+        String respons = controller.cancel(id);
+        System.out.println(respons);
     }
 
     //Registrator kan sjekke status for en henvendelse.
     private void sjekkHenvendelseStatus(){
-        System.out.println("Henvendelse_id du vil sjekke: ");
+        System.out.print("Henvendelse_id du vil sjekke: ");
         int id = Integer.parseInt(scanner.nextLine());
 
-        //String svar = controller.sjekkStatus(id);
-        //System.out.println(svar);
+        String respons = controller.sjekkStatus(id);
+        System.out.println(respons);
     }
+
 }
