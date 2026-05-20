c9009b4df363e57490f284ceeeb36dd829e45d09
Kneebone <kayern3@hotmail.com>
2026-05-20 14:03:19 +0200

    Mulighet for å "simulere" behandling av henvendelse

---
+++ src/klient/controller/BehandleHenvendelse.java
@@ -0,0 +1,22 @@
+package klient.controller;
+
+import common.Henvendelse;
+
+public class BehandleHenvendelse {
+    private final Henvendelse henvendelse;
+
+    public BehandleHenvendelse(Henvendelse henvendelse){
+        this.henvendelse = henvendelse;
+    }
+
+    public void behandleHenvendelse(){
+        try {
+            System.out.println("Starter behandlingen");
+            Thread.sleep(1000);
+            System.out.println("Henvendelse: " + henvendelse.getHenvendelseID() + " er ferdig behandlet");
+
+        } catch (InterruptedException e) {
+            System.out.println("Behandlingen av henvendelsen ble avbrutt");
+        }
+    }
+}
